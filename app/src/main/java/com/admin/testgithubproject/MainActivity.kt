7709023f32b32337baf1f04803390b64660a94cc
KostyaGig <kostiggig@list.ru>
2021-06-30 15:58:38 +0300

    Third commit for github

--- app/src/main/java/com/admin/testgithubproject/MainActivity.kt
+++ app/src/main/java/com/admin/testgithubproject/MainActivity.kt
@@ -3,10 +3,15 @@ package com.admin.testgithubproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+*  @author Zinoview
+**/
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("Hello github")
         print("Hello github")
     }
 }
